578ced72d2ca415c2da24444e18ba2d6b0f179c5
julianw113 <80606064+julianw113@users.noreply.github.com>
2021-07-02 12:58:32 -0500

    Update question2.c

--- question2.c
+++ question2.c
@@ -35,17 +35,16 @@ const char *semName = "/semLock";
 // if weird error with semaphore check if it is still left open here
 
 //structs
-struct processes{
+struct processes
+{
 	char name[5];
 	unsigned long long int cycleTime;
 	int memory;
 };
 
 //protoypes
-double scheduler2mem(sem_t *sem_id, int *numProcesses, struct processes prosArr2mem[]);
-double scheduler4mem(sem_t *sem_id, int *numProcesses, struct processes prosArr4mem[]);
-double scheduler8mem(sem_t *sem_id, int *numProcesses, struct processes prosArr8mem[]);
-void sortProcesses(struct processes prosArr[], struct processes prosArr2mem[], struct processes prosArr4mem[], struct processes prosArr8mem[]);
+double scheduler(sem_t *sem_id, int *numProcesses, struct processes prosArr[], int procPID[]);
+void sortProcesses(struct processes prosArr[]);
 void printPros(struct processes prosArr[]);
 void convertSectoDay(unsigned long long int totalTime);
 
@@ -57,19 +56,18 @@ int main()
 	int parentPid = getpid();
 	unsigned long long int tempCycle; 
 	int tempMem;
+	
 	//SHARED MEMORY
 	// time each processor spent running
 	double *totalTime = mmap(NULL, sizeof *totalTime, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	*totalTime = 0;
 	int *numProcesses = mmap(NULL, sizeof *numProcesses, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	*numProcesses = NUM_OF_PROCESSES;
+	int *procPID = mmap(NULL, 5*sizeof(procPID), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	
-	// array of processes, we will remove elements from the array as each processor takes on a process
+	// array of processes, we will remove elements from the array as each process takes on a process
 	struct processes* prosArr = mmap(NULL, NUM_OF_PROCESSES * sizeof(prosArr), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	struct processes* prosArr2mem = mmap(NULL, NUM_OF_PROCESSES * sizeof(prosArr2mem), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	struct processes* prosArr4mem = mmap(NULL, NUM_OF_PROCESSES * sizeof(prosArr4mem), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-	struct processes* prosArr8mem = mmap(NULL, NUM_OF_PROCESSES * sizeof(prosArr8mem), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-
+	
 	// Intializes random number generator
 	time_t t;
 	srand((unsigned) time(&t));
@@ -95,12 +93,13 @@ int main()
 	fclose(fp);
 	
 	// sort arr of structs
-	sortProcesses(prosArr, prosArr2mem, prosArr4mem, prosArr8mem);
+	//sortProcesses(prosArr);
+	
 	//test print
-	printPros(prosArr);
+	//printPros(prosArr);
 	
 	//create semaphore to be used for critical sections
-	sem_t *sem_id = sem_open(semName, O_CREAT, 0644, 1);
+	sem_t *sem_id = sem_open(semName, O_CREAT, 0644, 0);
 	if (sem_id == SEM_FAILED)
 	{
         perror("Parent  : [sem_open] Failed\n");
@@ -116,26 +115,24 @@ int main()
 	pid_t child_pid, wpid;
 	int status = 0;
 	fflush(0); // not directly relevant but always a good idea before forking
-	for (int i = 0; i < 5; i++) {
-		if (fork() == 0) {
+	for (int i = 0; i < 5; i++) 
+	{
+		if (fork() == 0) 
+		{
 		   	//call scheduling child function here
-			if(i < 2){
-		   	*totalTime += scheduler2mem(sem_id, numProcesses, prosArr2mem);
-			}
-			if(i >= 2 && i < 4){
-		   	*totalTime += scheduler4mem(sem_id, numProcesses, prosArr4mem);
-			*totalTime += scheduler2mem(sem_id, numProcesses, prosArr2mem);
-			}
-			if(i == 4){
-		   	*totalTime += scheduler8mem(sem_id, numProcesses, prosArr8mem);
-		   	*totalTime += scheduler4mem(sem_id, numProcesses, prosArr4mem);
-			*totalTime += scheduler2mem(sem_id, numProcesses, prosArr2mem);
-			}
+		   	procPID[i] = getpid();
+		   	*totalTime += scheduler(sem_id, numProcesses, prosArr, procPID);
 		   	exit(0);
 		}
 		// control reaches this point only in the parent
 	}
 	
+	//release control of semaphore
+	if (sem_post(sem_id) < 0)
+	{
+		printf("%d   : [sem_post] Failed \n", getpid());
+	}
+
 	// the parent waits for all the child processes to finish
 	while ((wpid = wait(&status)) > 0); 
 	
@@ -152,138 +149,151 @@ int main()
     return 0;
 }
 
-/*
- * functional loop where the 5 children processes will go into and begin to 
- * read the processes in randomProcesses.txt and then schdule which processor
- * will run which process
- */
-//scheduler for 2GB processes
-double scheduler2mem(sem_t *sem_id, int *numProcesses, struct processes prosArr2mem[]){
+//functional loop where the 5 children processes will enter and begin to read the 
+//processes in randomProcesses.txt, then schedule which processor will run which process
+double scheduler(sem_t *sem_id, int *numProcesses, struct processes prosArr[], int procPID[])
+{
 	double timeToRun = 0;       //keep track of wait time + execution time of everything that ran on this processor
 	double execTime = 0, wait = 0;
-	
-	while(prosArr2mem[0].name != 0){
+	int currentPID = getpid();
+
+	while (*numProcesses > 0)
+	{
+
+		if(prosArr[0].memory >= 400000 && currentPID == procPID[4])
+		{
 			// if semaphore available, then continue
 			if (sem_wait(sem_id) < 0)
+			{
 				printf("%d  : [sem_wait] Failed\n", getpid());
+			}
+
 			// THIS IS CRITICAL SECTION
 		
-			if (*numProcesses <= 0){
+			if (*numProcesses <= 0)
+			{
 				if (sem_post(sem_id) < 0)
+				{
 					printf("%d   : [sem_post] Failed \n", getpid());
+				}
 				return timeToRun + wait;
 			}
 		
-			// get next item in prosArr
-			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr2mem[0].name, prosArr2mem[0].cycleTime, prosArr2mem[0].memory, getpid());
-			memmove(&prosArr2mem[0], &prosArr2mem[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
+			// get next item in posArr
+			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr[0].name, prosArr[0].cycleTime, prosArr[0].memory, getpid());
 			*numProcesses = *numProcesses - 1;
 		
 			// calulate timeToRun
-			execTime = (double)prosArr2mem[0].cycleTime / GHZ;
+			execTime = (double)prosArr[0].cycleTime / GHZ;
 			wait += timeToRun;
 			timeToRun += execTime;
+		
+			//move start of prosArr
+			memmove(&prosArr[0], &prosArr[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
 	
 			//release control of semaphore
 			if (sem_post(sem_id) < 0)
+			{
 				printf("%d   : [sem_post] Failed \n", getpid());
-		
+			}
+
 			//sleep for last timeToRun /1000
-			usleep(execTime * 1000);
-
-	}	
+			usleep(execTime * 100);
+			continue;
+		}
 	
-	return timeToRun + wait;
-}
-
-//scheduler for 4GB processes
-double scheduler4mem(sem_t *sem_id, int *numProcesses, struct processes prosArr4mem[]){
-	double timeToRun = 0;       //keep track of wait time + execution time of everything that ran on this processor
-	double execTime = 0, wait = 0;
-	
-	while(prosArr4mem[0].name != 0){
+		if((prosArr[0].memory >= 200000 && prosArr[0].memory < 400000) && (currentPID == procPID[4] || currentPID == procPID[3] || currentPID == procPID[2]))
+		{
 			// if semaphore available, then continue
 			if (sem_wait(sem_id) < 0)
+			{
 				printf("%d  : [sem_wait] Failed\n", getpid());
+			}
+
 			// THIS IS CRITICAL SECTION
-		
-			if (*numProcesses <= 0){
+			if (*numProcesses <= 0)
+			{
 				if (sem_post(sem_id) < 0)
+				{
 					printf("%d   : [sem_post] Failed \n", getpid());
+				}
 				return timeToRun + wait;
 			}
 		
-			// get next item in prosArr
-			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr4mem[0].name, prosArr4mem[0].cycleTime, prosArr4mem[0].memory, getpid());
-			memmove(&prosArr4mem[0], &prosArr4mem[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
+			// get next item in posArr
+			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr[0].name, prosArr[0].cycleTime, prosArr[0].memory, getpid());
 			*numProcesses = *numProcesses - 1;
 		
 			// calulate timeToRun
-			execTime = (double)prosArr4mem[0].cycleTime / GHZ;
+			execTime = (double)prosArr[0].cycleTime / GHZ;
 			wait += timeToRun;
 			timeToRun += execTime;
+		
+			//move start of prosArr
+			memmove(&prosArr[0], &prosArr[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
 	
 			//release control of semaphore
 			if (sem_post(sem_id) < 0)
+			{
 				printf("%d   : [sem_post] Failed \n", getpid());
-		
+			}
+
 			//sleep for last timeToRun /1000
-			usleep(execTime * 1000);
+			usleep(execTime * 100);
+			continue;
+		}
 
-	}	
-	
-	return timeToRun + wait;
-}
-
-//scheduler for 8GB processes
-double scheduler8mem(sem_t *sem_id, int *numProcesses, struct processes prosArr8mem[]){
-	double timeToRun = 0;       //keep track of wait time + execution time of everything that ran on this processor
-	double execTime = 0, wait = 0;
-	
-	while(prosArr8mem[0].name != 0){
+		if(prosArr[0].memory < 200000)
+		{
 			// if semaphore available, then continue
 			if (sem_wait(sem_id) < 0)
+			{
 				printf("%d  : [sem_wait] Failed\n", getpid());
+			}
+
 			// THIS IS CRITICAL SECTION
 		
-			if (*numProcesses <= 0){
+			if (*numProcesses <= 0)
+			{
 				if (sem_post(sem_id) < 0)
+				{
 					printf("%d   : [sem_post] Failed \n", getpid());
+				}
 				return timeToRun + wait;
 			}
 		
-			// get next item in prosArr
-			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr8mem[0].name, prosArr8mem[0].cycleTime, prosArr8mem[0].memory, getpid());
-			memmove(&prosArr8mem[0], &prosArr8mem[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
+			// get next item in posArr
+			printf("%s  time: %llu   mem: %d   pid: %d\n", prosArr[0].name, prosArr[0].cycleTime, prosArr[0].memory, getpid());
 			*numProcesses = *numProcesses - 1;
 		
 			// calulate timeToRun
-			execTime = (double)prosArr8mem[0].cycleTime / GHZ;
+			execTime = (double)prosArr[0].cycleTime / GHZ;
 			wait += timeToRun;
 			timeToRun += execTime;
+		
+			//move start of prosArr
+			memmove(&prosArr[0], &prosArr[1], (NUM_OF_PROCESSES - 1) * sizeof(struct processes));
 	
 			//release control of semaphore
 			if (sem_post(sem_id) < 0)
+			{
 				printf("%d   : [sem_post] Failed \n", getpid());
-		
+			}
+
 			//sleep for last timeToRun /1000
-			usleep(execTime * 1000);
+			usleep(execTime * 100);
+		}
 	}	
-	
 	return timeToRun + wait;
 }
 
-/*
- * take in the arr of processes 
- * and sort based on time needed to run
- */
-void sortProcesses(struct processes prosArr[], struct processes prosArr2mem[], struct processes prosArr4mem[], struct processes prosArr8mem[])
+//take in the arr of processes and sort based on time needed to run
+void sortProcesses(struct processes prosArr[])
 {
 	int i, j;
 	int n = NUM_OF_PROCESSES;
 	struct processes key;
-	const char* nullPtr = '\0';
-	for (i = 1; i < n; i++)
+    for (i = 1; i < n; i++)
     { 
         key = prosArr[i]; 
         j = i - 1; 
@@ -298,42 +308,9 @@ void sortProcesses(struct processes prosArr[], struct processes prosArr2mem[], s
         } 
         prosArr[j + 1] = key; 
     } 
-
-	//distribute processes to memory limited arrays based on memory size of process
-	j = 0;
-	for (i = 0; i < n; i++)
-    { 
-        if(prosArr[i].memory < 200000)
-		{
-			prosArr2mem[j] = prosArr[i];
-			j++;
-		}  
-	}
-	j = 0;
-	for (i = 0; i < n; i++)
-    { 
-        if(prosArr[i].memory >= 200000 && prosArr[i].memory < 400000)
-		{
-			prosArr4mem[j] = prosArr[i];
-			j++;
-		}  
-	}
-	j = 0;
-	for (i = 0; i < n; i++)
-    { 
-        if(prosArr[i].memory >= 400000)
-		{
-			prosArr8mem[j] = prosArr[i];
-			j++;
-		}  
-	}
-printf("debug 2\n");	
-
 }
 
-/*
- * test print for the array of processes
- */
+//test print for the array of processes
 void printPros(struct processes prosArr[])
 {
 	int n = NUM_OF_PROCESSES;
@@ -343,13 +320,9 @@ void printPros(struct processes prosArr[])
 	}
 }
 
-/*
- * converts seconds to
- * day, hour, minute, second
- */
+//converts seconds to day, hour, minute, second
 void convertSectoDay(unsigned long long int n)
 {
-	
     unsigned int day = n / (24 * 3600);
  
     n = n % (24 * 3600);
@@ -362,7 +335,7 @@ void convertSectoDay(unsigned long long int n)
     unsigned int seconds = n;
     
 	printf("days: %d\n", day);
-	printf("hours: %d\n", hour);
+	printf("hour: %d\n", hour);
 	printf("minutes: %d\n", minutes);
 	printf("seconds: %d\n", seconds);
 }
